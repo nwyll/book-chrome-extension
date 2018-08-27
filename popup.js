2e944a22d004cf4a9b5dd200b846aa602d37042c
Natalie Wyll <nataliewyll@yahoo.com>
2018-08-27 20:59:51 +0200

    refactored handleSearch

--- popup.js
+++ popup.js
@@ -3,20 +3,51 @@ const buildBookItemTemplate = function(coverImage, title, publishedDate) {
       `<div class="book">`
     + `   <img src=${coverImage} alt="${title}" class="cover-img"/>`
     + `   <div class="published-date">${publishedDate}</div>`
-    + `<button type="button" 
-               onclick="handleAddToWatchList(event)"
-               data-title="${title}"
-               data-publishedDate=${publishedDate}>Add to Watch List!</button>`
+    + `   <button type="button" 
+            onclick="handleAddToWatchList(event)"
+            data-title="${title}"
+            data-publishedDate=${publishedDate}>
+              Add to Watch List!
+          </button>`
     + `</div>`
   ;
 
   return template; 
 }
 
+const handleSearch = function() {
+  event.preventDefault();
+  $("#content").empty();
+
+  const BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
+  const OPTIONS = '&orderBy=newest'
+
+  let query = $("#query").val();
+  $("#query").val(" ");
+
+  if(query) {
+    fetch(`${BASE_URL}${query}${OPTIONS}`, { method: 'GET'} )
+      .then(response => response.json())
+      .then(json => {
+        let { items } = json;
+      
+        items.map((item) => {
+          const title = item.volumeInfo.title,
+                authors = item.volumeInfo.authors, 
+                publishedDate = item.volumeInfo.publishedDate,
+                imageLinks = item.volumeInfo.imageLinks,
+                coverImage = imageLinks !== undefined ? imageLinks.thumbnail : 'notebook.png';
+
+          let bookItem = buildBookItemTemplate(coverImage, title, publishedDate);
+           $("#content").append(bookItem);
+        });
+      });
+  }
+};
+
 const createAlarm = function(title, publishedDate) {
   console.log("alarm created", {title}, {publishedDate})
 
-  
   chrome.alarms.create(title, {
     when: Date.now() + 1000 });
     //when: Date.parse(publishedDate)});
@@ -30,48 +61,8 @@ const handleAddToWatchList = function(event) {
   createAlarm(title, publishedDate);
 };
 
-
 $(document).ready(function(){
-
-  //handleSearch
-  $("#searchForm").submit(function() {
-    event.preventDefault();
-
-    //clear content div
-    $("#content").empty();
-
-    const BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
-    const OPTIONS = '&orderBy=newest'
-
-    //get query value from user
-    let query = $("#query").val();
-    //clear user input
-    $("#query").val(" ");
-
-    //a little validation
-    if(query) {
-      //GET response from Google Books API
-      fetch(`${BASE_URL}${query}${OPTIONS}`, { method: 'GET'} )
-        .then(response => response.json())
-        .then(json => {
-          let { items } = json;
-        
-          //for each item returned add book to content div
-          items.map((item) => {
-            const title = item.volumeInfo.title,
-                  authors = item.volumeInfo.authors, 
-                  publishedDate = item.volumeInfo.publishedDate,
-                  imageLinks = item.volumeInfo.imageLinks,
-                  coverImage = imageLinks !== undefined ? imageLinks.thumbnail : 'notebook.png';
-
-            let bookItem = buildBookItemTemplate(coverImage, title, publishedDate);
-             $("#content").append(bookItem);
-          });
-        });
-    }
-
-    return false;
-  });
+  $("#searchForm").submit(handleSearch);
 
   return false; 
 });
