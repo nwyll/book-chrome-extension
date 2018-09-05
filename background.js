b285dcbf7b20a5516fb438813efa0a6e167969ee
Natalie Wyll <nataliewyll@yahoo.com>
2018-09-05 18:35:50 +0200

    delete book from watch list automatically one alarm/notification fires

--- background.js
+++ background.js
@@ -1,16 +1,20 @@
 chrome.alarms.onAlarm.addListener(function (alarm) {
   const id = alarm.name;
   
+  //get book info from storage
   chrome.storage.sync.get(id, function (result) {
     const bookObj = result[id];
 
     const options = {
       type: 'basic',
       title: bookObj.title,
-      message: `Don't forget ${bookObj.title} comes out on ${bookObj.publishedDate}!`,
+      message: `Don't forget ${bookObj.title} comes out TODAY!`,
       iconUrl: 'search32.png'
     };
 
     chrome.notifications.create(options);
+
+    //delete book from storage
+    chrome.storage.sync.remove(id);
   });
 });
